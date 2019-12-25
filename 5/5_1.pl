2a58afb0a968ca5f8da4bdef03edc1e842f3cbb3
James Green <jkg@earth.li>
2019-12-25 19:43:20 +0000

    Day 5 Puzzle 1 implemented, opcode handling is getting sligtly cleaner maybe?

--- 5/5_1.pl
+++ 5/5_1.pl
@@ -2,42 +2,67 @@
 
 use strict; use warnings;
 use Modern::Perl;
+use integer;
+use feature 'say';
 
 our $pc = 0;
 our @ram = split /,/, <>;
 
-# HOTFIX
-$ram[1] = 12;
-$ram[2] = 2;
-
-
 while (1) {
-	if ( $ram[$pc] == 1 ) {
-		add();
+	my $instruction = $ram[$pc] % 100;
+	my @modes = reverse split //, $ram[$pc] / 100;
+
+	if ( $ram[$pc] % 100 == 1 ) {
+		add(@modes);
 		$pc+=4;
 		next;
-	} elsif ( $ram[$pc] == 2 ) {
-		mult();
+	} elsif ( $ram[$pc] % 100 == 2 ) {
+		mult(@modes);
 		$pc+=4;
 		next;
-	} elsif ( $ram[$pc] == 99 ) {
+	} elsif ( $ram[$pc] % 100 == 3 ) {
+		my $input = <>;
+		store($input);
+		$pc += 2;
+	} elsif ( $ram[$pc] % 100 == 4 ) {
+		output();
+		$pc += 2;
+	} elsif ( $ram[$pc] % 100 == 99 ) {
 		last;
 	} else {
 		die "Unexpected opcode found at position $pc";
 	}
 }
 
-use DDP;
-p @ram;
-
 sub add {
+	my $mode1 = shift // 0;
+	my $mode2 = shift // 0;
 	my ( $first, $second, $output ) = @ram[$pc+1..$pc+3];
-	$ram[$output] = $ram[$first] + $ram[$second];
+
+	$first = $ram[$first] unless $mode1;
+	$second = $ram[$second] unless $mode2;
+
+	$ram[$output] = $first + $second;
 }
 
 sub mult {
-    my ( $first, $second, $output ) = @ram[$pc+1..$pc+3];
-    $ram[$output] = $ram[$first] * $ram[$second];
+	my $mode1 = shift // 0;
+	my $mode2 = shift // 0;
+	my ( $first, $second, $output ) = @ram[$pc+1..$pc+3];
+
+	$first = $ram[$first] unless $mode1;
+	$second = $ram[$second] unless $mode2;
+
+	$ram[$output] = $first * $second;
 }
 
+sub store {
+	my $location = $ram[$pc+1];
+	$ram[$location] = shift;
+}
+
+sub output {
+	my $location = $ram[$pc+1];
+	say $ram[$location];
+}
 
