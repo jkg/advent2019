3d1fdbcce31792fb28ea79f5a3f4e514c4ecc6bb
James Green <jkg@earth.li>
2019-12-25 18:52:44 +0000

    Day 4 puzzle 1 complete

---
+++ 4_1.pl
@@ -0,0 +1,33 @@
+#!perl
+
+use strict;
+use warnings;
+
+my $min = 248345;
+my $max = 746315;
+
+my $count;
+
+for ( $min .. $max ) {
+	my @digits = split //,$_;
+
+	my $double = 0;
+	my $ascend = 0;
+
+	# do stuff
+	
+	for ( 1..5 ) {
+		if ( $digits[$_] < $digits[$_-1] ) {
+			$ascend = 1;
+			last;
+		}
+		if ( not $double and $digits[$_] == $digits[$_-1] ) {
+			$double = 1;
+		}
+	}
+
+	$count++ if $double and not $ascend;
+}
+
+print "count is $count";
+
